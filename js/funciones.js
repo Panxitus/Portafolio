314d986aa09bd1befd9215de4e5acc98af4d82e7
Panxitus <gonzalezuribe.francisco@gmail.com>
2024-03-20 14:07:38 -0300

    funcionesJS

--- js/funciones.js
+++ js/funciones.js
@@ -4,8 +4,24 @@ jQuery(document).ready(listo);
 
 function listo(){
     jQuery(".hamb").click(function(e){
-        e.preventDefault("header .container nav").toggleClass(open);
+        e.preventDefault();
+        jQuery("header .container nav").toggleClass(open);
+        jQuery(".hamb i").toggleClass(fa-x);
     });
+
+    jQuery ("header .container nav a").click (function(){
+        jQuery("header .container nav").removeClass(open);
+        jQuery(".hamb i").removeClass(fa-x);
+        var dev =jQuery(this).attr(href);
+        jQuery("html,body").animate({
+            "scrollTop" : jQuery(dev).offset().top
+        })
+        // alert(dev);
+    })
+
+    
+
+
 }
 
 
